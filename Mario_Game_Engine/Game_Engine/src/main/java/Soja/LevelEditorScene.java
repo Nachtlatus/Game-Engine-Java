f04ca8493c1736021671869a00836b797431967a
Nachtlatus <Valentin.saizmunteanu@gmail.com>
2024-06-23 14:58:50 +0200

    Safe

---
+++ Mario_Game_Engine/Game_Engine/src/main/java/Soja/LevelEditorScene.java
@@ -0,0 +1,18 @@
+package Soja;
+
+public class LevelEditorScene extends Scene {
+
+
+    public LevelEditorScene() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+        System.out.println("" + (1.0f / dt) + "FPS");
+
+
+    }
+
+}
--- Mario_Game_Engine/Game_Engine/src/main/java/Soja/Window.java
+++ Mario_Game_Engine/Game_Engine/src/main/java/Soja/Window.java
@@ -1,5 +1,6 @@
 package Soja;
 
+import Soja.Util.Time;
 import org.lwjgl.Version;
 import org.lwjgl.glfw.GLFWErrorCallback;
 import org.lwjgl.opengl.GL;
@@ -17,13 +18,38 @@ public class Window {
     private String title;
     private long glfwWindow;
 
+    public float r,g , b, a;
+    private boolean fadeToBlack = false;
+
     private static Window window = null;
+
+    private static Scene currentScene;
 
     private Window()    {
             this.width = 1920;
             this.height = 1080;
             this.title = "Mario";
+            r = 1;
+            b = 1;
+            g = 1;
+            a = 1;
 
+    }
+
+    public static void changeScene(int newScene) {
+        switch (newScene) {
+            case 0:
+               currentScene = new LevelEditorScene();
+               currentScene.init();
+                break;
+            case 1:
+                currentScene = new LevelScene();
+                currentScene.init();
+                break;
+            default:
+                assert false : "Unknown scene'" + newScene + "'";
+                break;
+        }
     }
 
     public static Window get() {
@@ -91,21 +117,33 @@ public class Window {
         // creates the GLCapabilities instance and makes the OpenGL
         // bindings available for use.
         GL.createCapabilities();
+
+        Window.changeScene(0);
     }
 
     public void loop() {
+
+        float beginTime = Time.getTime();
+        float endTime = Time.getTime();
+        float dt = -1.0f;
+
         while (!glfwWindowShouldClose(glfwWindow)) {
             //Poll events
             glfwPollEvents();
 
-            glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+            glClearColor(r, g, b, a);
             glClear(GL_COLOR_BUFFER_BIT);
 
-            if (KeyListener.isKeyPressed(GLFW_KEY_SPACE)) {
-                System.out.println("Space key is pressed");
+            if (dt >= 0) {
+                currentScene.update(dt);
             }
 
             glfwSwapBuffers(glfwWindow);
+
+            endTime = Time.getTime();
+            dt = endTime - beginTime;
+            beginTime = endTime;
+
         }
     }
 }
